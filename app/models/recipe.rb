8a500de95d457c5c7924a9b345a9229f98b8915a
Erika Portilla <ewi_mp@McMac.local>
2016-08-02 22:02:11 -0700

    Finished requirements for course

---
+++ app/models/recipe.rb
@@ -0,0 +1,15 @@
+class Recipe
+	include HTTParty
+
+	hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
+	key_value = ENV["FOOD2FORK_KEY"]
+	base_uri "http://#{hostport}/api"
+	default_params key: key_value
+	format :json
+
+	def self.for recipe
+		get("/search", query: { q: recipe })["recipes"]
+		#recipes is the json element name
+	end
+
+end
