101f818d4047e3279d26383f08de44bd2836353a
Erika Portilla <ewi_mp@McMac.local>
2016-08-27 18:29:31 -0700

    Added pagination

--- Gemfile
+++ Gemfile
@@ -26,3 +26,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'httparty'
+
+gem 'will_paginate'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,9 @@
 class RecipesController < ApplicationController
+	require 'will_paginate/array'
+
   def index
   	@search = params[:search] || "chocolate"
-  	@recipes = Recipe.for(@search) || []
+  	@search_for = Recipe.for(@search)|| []
+  	@recipes = @search_for.paginate(page: params[:page], per_page: 5)
   end
 end
