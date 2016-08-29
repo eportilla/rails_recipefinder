188ccdc29722ecb4b316397bc5d4f3b9d4621095
Erika Portilla <ewi_mp@McMac.local>
2016-08-28 19:11:27 -0700

    Added styles for search bar, changed search default value condition from || to .blank?

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,13 @@ class RecipesController < ApplicationController
 	require 'will_paginate/array'
 
   def index
-  	@search = params[:search] || "chocolate"
-  	@search_for = Recipe.for(@search)|| []
+  	# Rails method to check nil, empty
+  	if params[:search].blank?
+  		@search = "chocolate" # default value
+  	else
+  		@search = params[:search]
+  	end
+  	@search_for = Recipe.for(@search)|| [] # if returns nil is emty array
   	@recipes = @search_for.paginate(page: params[:page], per_page: 5)
   end
 end
